56ac6db21644ea0a4a801d68de7c66169ffd4d9d
Bastien <bastien.couder@gmail.com>
2024-01-24 18:46:45 +0100

    v2

--- client/src/schemas/index.ts
+++ client/src/schemas/index.ts
@@ -4,49 +4,85 @@ const messageChampRequis = "Ce champ est requis";
 const messageMinRequis = "Au moins un élément est requis";
 
 const educationSchema = z.object({
-  diplome: z.string().nonempty({ message: messageChampRequis }),
-  date: z.string().nonempty({ message: messageChampRequis }),
-  lieu: z.string().nonempty({ message: messageChampRequis }),
+  diplome: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
+  date: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
+  lieu: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
 });
 
 const experienceSchema = z.object({
-  date: z.string().nonempty({ message: messageChampRequis }),
-  lieu: z.string().nonempty({ message: messageChampRequis }),
-  description: z.string().nonempty({ message: messageChampRequis }),
+  date: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
+  lieu: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
+  description: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
 });
 
 const languageSchema = z.object({
-  language: z.string().nonempty({ message: messageChampRequis }),
-  level: z.string().nonempty({ message: messageChampRequis }),
+  language: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
+  level: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
 });
 
 const socialnetworkSchema = z.object({
-  linkedin: z.string().optional(),
-  github: z.string().optional(),
-  instagram: z.string().optional(),
-  website: z.string().optional(),
+  linkedin: z.string({ required_error: messageChampRequis }).optional(),
+  github: z.string({ required_error: messageChampRequis }).optional(),
+  instagram: z.string({ required_error: messageChampRequis }).optional(),
+  website: z.string({ required_error: messageChampRequis }).optional(),
 });
 
 export const pdfDataSchema = z.object({
-  prenom: z.string().nonempty({ message: messageChampRequis }),
-  nom: z.string().nonempty({ message: messageChampRequis }),
-  status: z.string().nonempty({ message: messageChampRequis }),
+  prenom: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
+  nom: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
+  status: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
   email: z
-    .string()
+    .string({ required_error: messageChampRequis })
     .email({ message: "Adresse email invalide" })
     .nonempty({ message: messageChampRequis }),
-  tel: z.string().nonempty({ message: messageChampRequis }),
+  tel: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
   avatar: z.string().optional(),
-  adresse: z.string().nonempty({ message: messageChampRequis }),
-  profil: z.string().nonempty({ message: messageChampRequis }),
+  adresse: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
+  profil: z
+    .string({ required_error: messageChampRequis })
+    .nonempty({ message: messageChampRequis }),
   educations: z.array(educationSchema).min(1, { message: messageMinRequis }),
   experiences: z.array(experienceSchema).min(1, { message: messageMinRequis }),
   competences: z
-    .array(z.string().nonempty({ message: messageChampRequis }))
+    .array(
+      z
+        .string({ required_error: messageChampRequis })
+        .nonempty({ message: messageChampRequis })
+    )
     .min(1, { message: messageMinRequis }),
   languages: z.array(languageSchema),
   hobbies: z
-    .array(z.string().nonempty({ message: messageChampRequis }))
+    .array(
+      z
+        .string({ required_error: messageChampRequis })
+        .nonempty({ message: messageChampRequis })
+    )
     .min(1, { message: messageMinRequis }),
   socialnetwork: z.array(socialnetworkSchema),
 });
